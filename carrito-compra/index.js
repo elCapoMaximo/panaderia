477d3a9db7cb8453697eb45b00b641e5e94be228
elCapoMaximo <Masterhaqer324@gmail.com>
2023-03-31 19:34:12 -0300

    agregar panaderia commit

---
+++ carrito-compra/index.js
@@ -0,0 +1,6 @@
+const btnCant = document.querySelector('.container-icon')
+const cantProductos = document.querySelector('.contador-cant-productos')
+
+btnCant.addEventListener('click',() => {cantProductos.classList.toggle('ocultar-cant')})
+
+
