d6d7c8056abebc8617b220842ea80a320a9330ed
elCapoMaximo <Masterhaqer324@gmail.com>
2023-04-03 18:26:54 -0400

    cambio final

---
+++ index.js
@@ -0,0 +1,5 @@
+function mostrarMensaje() {
+    document.getElementById("mensaje").style.display = "block";
+    document.getElementById("mensaje").innerHTML = "Gracias por tu mensaje. Ha sido enviado!!";
+  }
+  
